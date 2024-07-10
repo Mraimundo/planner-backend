6f3304ec1245b772e452a18bc208d1a132861f0d
Mraimundo <mdumundo18@gmail.com>
2024-07-10 01:34:42 -0300

    feat: create confirm trip

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,26 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { createTrip } from "./routes/create-trip";
+import { confirmTrip } from "./routes/confirm-trip";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-app.listen({ port: 333 }).then(() => {
+app.register(cors, {
+  origin: "*",
+});
+
+app.setSerializerCompiler(serializerCompiler);
+app.setValidatorCompiler(validatorCompiler);
+
+app.register(createTrip);
+app.register(confirmTrip);
+
+app.listen({ port: 3333 }).then(() => {
   console.log("Server running!");
 });
